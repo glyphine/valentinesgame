2a8734fd0655ace701c3d7281f1cdcaee4b1978d
Gwynne <lobsgwynne37@gmail.com>
2026-09-04 23:43:20 +0800

    update background image in gwyn.html and format vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite"; 
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
     base: "./",
-    build: { 
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                startgame: resolve(__dirname, "startgame.html"),
+                valentine: resolve(__dirname, "valentine.html"),
+                gwyn: resolve(__dirname, "gwyn.html"),
+                gameover: resolve(__dirname, "gameover.html"),
+            },
+        },
         minify: "terser",
     },
 });
